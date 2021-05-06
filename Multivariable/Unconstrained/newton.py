20fe48a9c02ee7a0a374566536906b9736f9ad81
sartaajkhan <67485363+sartaajkhan@users.noreply.github.com>
2021-05-06 05:39:59 -0400

    Update newton.py

--- Multivariable/Unconstrained/newton.py
+++ Multivariable/Unconstrained/newton.py
@@ -53,3 +53,54 @@ def NewtonMethod(func,X,Y):
         pass
     
     return {'optimum solution: ': solutionC, 'error: ': mean_error}
+
+def Marquardt(func,X,Y):
+    """
+    apply Marquardt algorithm to determine optimum value of f = f(X1,X2,...,XN)
+    func - objective function (str)
+    X - list of string variables (list) {x1,x2,..,xN}
+    Y - initial guess (list)
+    
+    The Marquardt algorithm is a combination of gradient descent and Newton's method.
+    This is version 1 of the Marquardt algorithm; this algorithm version proposes that:
+    X(i+1) = X(i) - Hinv*grad(F).transpose
+        if f[x(k+1)] < f[x(k)]: gamma(k+1) = gamma(k)/2
+        else: gamma(k+1) = 2*gamma(k)
+    
+    Version 2 will utilize the following development:
+    H_bar = H + gamma*I; I = identity matrix, H = Hessian matrix, H_bar = approximate Hessian
+    at first: utilize large values of gamma; H_bar = gamma*I; H_bar.inv = (1/gamma)*I
+    as you get closer to optimum, use Newton's method definition; H_bar = H
+    """
+    gamma = 10
+    solutionC = []
+    mean_error = 1
+    guess = Y
+    error = []
+    
+    while mean_error > 1e-12:
+        H = Hessian(func,X,guess)
+        Hinv = inverseMatrix(H)
+        gradF = grad(func,X,guess)
+        
+        HinvdF = np.matmul(Hinv,gradF).tolist()
+        solutionC = list(map(sub,guess,HinvdF))
+        
+        f1 = multiOut(func,X,guess)
+        f2 = multiOut(func,X,solutionC)
+        
+        if f2 < f1:
+            gamma = gamma/2 #f[x(k+1)] < f[x(k)]; gamma(k+1) = gamma(k)/2
+        elif f2 > f1:
+            gamma = 2*gamma #f[x(k+1)] > f[x(k)]; gamma(k+1) = 2*gamma(k)
+        
+        
+        del error[:]
+        for k in range(0,len(solutionC)):
+            e = abs((solutionC[k]-guess[k])/guess[k])
+            error.append(e)
+        
+        guess = solutionC
+        mean_error = statistics.mean(error)
+        
+    return solutionC
